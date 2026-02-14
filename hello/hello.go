894f1dade7a079854b5cf65fa3a7a148a2d22722
prasagungg <agungprasetio878@gmail.com>
2026-02-14 23:52:34 +0700

    multiple greting

--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Agung")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	names := []string{"Agung", "Prasetio", "Baduyut"}
+
+	message, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
